c82505610b7e59ca4a024808b5c61f4ca6c40a80
Hums Shaik <humsshaik@gmail.com>
2021-04-13 14:58:17 -0500

    changes 6

---
+++ UFO-Level-1/static/js/app1.js
@@ -0,0 +1,17 @@
+// from data.js
+var tableData = data;
+var tbody = d3.select("tbody");
+
+// Display the entire dataset as default
+
+tableData.forEach((report) => {
+    console.log(report);
+    var row = tbody.append('tr');
+
+    Object.entries(report).forEach(([key, value]) => {
+        console.log(key, value);
+        var cell = row.append('td');
+        cell.text(value);
+    });
+});
+
